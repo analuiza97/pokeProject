e0e4e98efc4af2ae63f9de5c0cf5348fb05e2872
Ana Luíza Fontes <analuizaffontes97@gmail.com>
2021-01-08 20:55:27 -0300

    feat(components): typelabel component

---
+++ components/atoms/TypeLabel/TypeLabel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TypeLabel = () => {
+    return (
+        <div>
+            Ooooi
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { TypeLabel } from '../components/atoms/TypeLabel'
 
 export default function index() {
     return (
         <div>
-            Hello World!
+            <TypeLabel/>
         </div>
     )
 }
