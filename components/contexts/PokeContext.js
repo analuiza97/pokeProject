aa58e39334e51e126d9aa0db84ad3d12c5fd466c
Ana Luíza Fontes <analuizaffontes97@gmail.com>
2021-01-11 18:27:06 -0300

    feat(components): pokecontext context

---
+++ components/contexts/PokeContext.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { getAllPokemons } from "../../utils/api/PokemonApi";
+
+const PokemonsContext = React.createContext([[], () => {}]);
+
+const PokemonsProvider = (props) => {
+  const [state, setState] = useState([]);
+  
+  useEffect(() => {
+    getAllPokemons().then((data) => setState(data.data.results));
+  }, []);
+
+  return (
+    <PokemonsContext.Provider value={[state, setState]}>
+      {props.children}
+    </PokemonsContext.Provider>
+  );
+};
+
+export { PokemonsContext, PokemonsProvider };
